682ffd861bf091a63d0b67845f4a85bf2a333a2a
sunyingjian <sunyingjian@outlook.com>
2022-04-16 10:57:13 +0800

    完善MLP

--- Work/MLP_Run.py
+++ Work/MLP_Run.py
@@ -4,11 +4,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import init_ui
 import pandas as pd
-from sklearn.svm import SVC
+import tensorflow as tf
 import sklearn.metrics as metrics
 import pandas as pd
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 
 def data_process(train_path,validation_path,select_features,pre_feature):
@@ -21,25 +20,10 @@ def data_process(train_path,validation_path,select_features,pre_feature):
     data = {'X_train':train_data_x,'y_train':train_data_y,'X_valid':valid_data_x,'y_valid':valid_data_y}
     return data
 
-def train_useSVM(data,options):
-    # C：错误项的惩罚系数。C越大，即对分错样本的惩罚程度越大，因此在训练样本中准确率越高，但是泛化能力降低，也就是对测试数据的分类准确率降低。相反，减小C的话，容许训练样本中有一些误分类错误样本，泛化能力强。对于训练样本带有噪声的情况，一般采用后者，把训练样本集中错误分类的样本作为噪声。
-    # kernel='linear'时，为线性核，C越大分类效果越好，但有可能会过拟合（defaul C=1）。
-    # kernel='rbf'时（default），为高斯核，gamma值越小，分类界面越连续；gamma值越大，分类界面越“散”，分类效果越好，但有可能会过拟合。
-    # kernel='poly'时，多项式函数,degree 表示多项式的程度-----支持非线性分类。更高gamma值，将尝试精确匹配每一个训练数据集，可能会导致泛化误差和引起过度拟合问题。
-    # kernel='sigmoid'时，支持非线性分类。更高gamma值，将尝试精确匹配每一个训练数据集，可能会导致泛化误差和引起过度拟合问题。
-    # gamma：float参数 默认为auto。核函数系数，只对‘rbf’,‘poly’,‘sigmod’有效。如果gamma为auto，代表其值为样本特征数的倒数，即1/n_features.
-    # decision_function_shape='ovr'时，为one v rest，即一个类别与其他类别进行划分，
-    # decision_function_shape='ovo'时，为one v one，即将类别两两之间进行划分，用二分类的方法模拟多分类的结果。
-    # probability：bool参数 默认为False，是否启用概率估计。 这必须在调用fit()之前启用，并且会fit()方法速度变慢。
-    # cache_size：float参数 默认为200，指定训练所需要的内存，以MB为单位，默认为200MB。
-    # class_weight：字典类型或者‘balance’字符串。默认为None，给每个类别分别设置不同的惩罚参数C，如果没有给，则会给所有类别都给C=1，即前面参数指出的参数C.如果给定参数‘balance’，则使用y的值自动调整与输入数据中的类频率成反比的权重。
-    # max_iter ：int参数 默认为-1，最大迭代次数，如果为-1，表示不限制
-    model = make_pipeline(StandardScaler(),SVC(C=options['C'],
-                kernel=options['kernel'],
-                gamma=options['gamma'],
-                decision_function_shape=options['decision_function_shape']))
-    model.fit(data['X_train'],data['y_train'])
+def train_useMLP(data,options):
+    model = tf.keras.models.load_model(f'DL\\mlp\\best_weight{options[0]} {options[1]} {options[2]}.h5')
     y_pred = model.predict(data['X_valid'])
+    y_pred = np.array([c.argmax() for c in y_pred])
     acc = metrics.precision_score(data['y_valid'],y_pred,average='micro')
     f1_score = metrics.f1_score(data['y_valid'],y_pred,average='micro')
     recall_score = metrics.recall_score(data['y_valid'],y_pred,average='micro')
@@ -56,50 +40,37 @@ class Downleft(init_ui.downleft):
     def tab3UI(self):
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
-        self.label_C = QLabel('惩罚系数C')
-        self.label_C.setEnabled(True)
-        self.spinBox = QDoubleSpinBox()
-        self.spinBox.setEnabled(True)
-        self.spinBox.setPrefix("")
-        self.spinBox.setMinimum(0.0)
-        self.spinBox.setMaximum(10.1)
-        self.spinBox.setSingleStep(0.1)
-        self.spinBox.setProperty("value", 1.0)
-        self.spinBox.setObjectName("spinBox")
-        self.label_kernal = QLabel("Kernal")
-        self.label_kernal.setEnabled(True)
-        self.combox_kernal = QComboBox()
-        self.combox_kernal.setEnabled(True)
-        self.combox_kernal.addItem("")
-        self.combox_kernal.addItem("")
-        self.combox_kernal.addItem("")
-        self.combox_kernal.addItem("")
-        self.combox_kernal.setCurrentText("rbf")
-        self.combox_kernal.setItemText(0, "linear")
-        self.combox_kernal.setItemText(1, "poly")
-        self.combox_kernal.setItemText(2, "rbf")
-        self.combox_kernal.setItemText(3, "sigmoid")
-        self.label_gamma = QLabel("Gamma")
-        self.label_gamma.setEnabled(True)
-        self.combox_gamma = QComboBox()
-        self.combox_gamma.setEnabled(True)
-        self.combox_gamma.addItem("")
-        self.combox_gamma.addItem("")
-        self.combox_gamma.setItemText(0, "auto")
-        self.combox_gamma.setItemText(1, "scale")
-        self.label_DFS = QLabel("DFS")
-        self.label_DFS.setEnabled(True)
-        self.combox_DFS = QComboBox()
-        self.combox_DFS.setEnabled(True)
-        self.combox_DFS.addItem("")
-        self.combox_DFS.addItem("")
-        self.combox_DFS.setItemText(0, "ovr")
-        self.combox_DFS.setItemText(1, "ovo")
+        self.label_lr = QLabel("learning rate")
+        self.label_lr.setEnabled(True)
+        self.combox_lr = QComboBox()
+        self.combox_lr.setEnabled(True)
+        self.combox_lr.addItem("")
+        self.combox_lr.addItem("")
+        self.combox_lr.setCurrentText("1e-3")
+        self.combox_lr.setItemText(0, "1e-3")
+        self.combox_lr.setItemText(1, "1e-2")
+        self.label_epochs = QLabel("epochs")
+        self.label_epochs.setEnabled(True)
+        self.combox_epochs = QComboBox()
+        self.combox_epochs.setEnabled(True)
+        self.combox_epochs.addItem("")
+        self.combox_epochs.addItem("")
+        self.combox_lr.setCurrentText("20")
+        self.combox_epochs.setItemText(0, "20")
+        self.combox_epochs.setItemText(1, "50")
+        self.label_denses = QLabel("denses")
+        self.label_denses.setEnabled(True)
+        self.combox_denses = QComboBox()
+        self.combox_denses.setEnabled(True)
+        self.combox_denses.addItem("")
+        self.combox_denses.addItem("")
+        self.combox_denses.setCurrentText("3")
+        self.combox_denses.setItemText(0, "2")
+        self.combox_denses.setItemText(1, "3")
         formLayout = QFormLayout()
-        formLayout.addRow(self.label_C,self.spinBox)
-        formLayout.addRow(self.label_kernal,self.combox_kernal)
-        formLayout.addRow(self.label_gamma,self.combox_gamma)
-        formLayout.addRow(self.label_DFS,self.combox_DFS)
+        formLayout.addRow(self.label_lr,self.combox_lr)
+        formLayout.addRow(self.label_epochs,self.combox_epochs)
+        formLayout.addRow(self.label_denses,self.combox_denses)
         hbox.addLayout(formLayout)
         vbox.addLayout(hbox)
         self.tab3.setLayout(vbox)
@@ -109,7 +80,7 @@ class newMainWindow(QWidget):
     def __init__(self):
         super(newMainWindow, self).__init__()
         self.initUI()
-        self.setObjectName('SVM')
+        self.setObjectName('MLP')
 
     def initUI(self):
         # self.widget = QWidget()
@@ -239,16 +210,12 @@ class newMainWindow(QWidget):
             datafile_train = './data/' + self.downleft.data_train
             datafile_test = './data/' + self.downleft.data_val
             data_test = pd.read_csv(datafile_train)
-            C = self.downleft.spinBox.value()
-            kernel = self.downleft.combox_kernal.currentText()
-            gamma = self.downleft.combox_gamma.currentText()
-            decision_function_shape = self.downleft.combox_DFS.currentText()
-            options = {'C': C,
-                       'kernel': kernel, 'gamma': gamma,
-                       'decision_function_shape': decision_function_shape
-                       }
+            lr = self.downleft.combox_lr.currentText()
+            epochs = self.downleft.combox_epochs.currentText()
+            denses = self.downleft.combox_denses.currentText()
+            options = (denses, epochs, lr)
             data = data_process(datafile_train, datafile_test, self.downleft.feature_selected, self.downleft.feature_pre)
-            res = train_useSVM(data, options)
+            res = train_useMLP(data, options)
             self.downright.textLine_acc.setText(str(res['acc']))
             self.downright.textLine_recall.setText(str(res['recall_score']))
             self.downright.textLine_f1.setText(str(res['f1_score']))
--- Work/first-syj.py
+++ Work/first-syj.py
@@ -11,6 +11,7 @@ import RFR_Run
 import GBDT_Run
 import GBDTR_Run
 import MLR_Run
+import MLP_Run
 import Ridge_Run
 import TreeR_Run
 # import LSTM_Run
@@ -218,6 +219,7 @@ class Main_Win(QMainWindow):
 
         btn_dnn = QPushButton('全连接神经网络')
         self.fontui(btn_dnn)
+        btn_dnn.clicked.connect(lambda :self.Add_Page(MLP_Run.newMainWindow()))
 
         btn_lstm = QPushButton('LSTM模型')
         self.fontui(btn_lstm)
